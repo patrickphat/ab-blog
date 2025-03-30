a3a146f5be45aac200691ac7479cbed216f1c332
Patrick <me@patrickphat.com>
2025-03-30 20:51:44 +0700

    feat: update blog theme and add new content
    - Add new blog post about industrial heating technologies
    - Update site config with new author, title, and description
    - Replace indigo theme with green across components
    - Add new ServicesCTA component for promoting services
    - Update categories to reflect new blog focus areas
    - Add new images for blog posts and branding
    - Introduce new Navbar component for better navigation

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,9 +11,10 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Alpha Bits', // Site author
+	title: 'Alpha Bits Blog', // Site title.
+	description:
+		'Get top trends with AI, Data, IoT, and Software - Stay ahead with the latest insights and developments in technology.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
